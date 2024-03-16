3902f7f9cca46d60af522769d6ac1ec8ac20e4b2
Ivan Titov <85026038+ivanstiv@users.noreply.github.com>
2024-03-16 08:32:13 +0300

    Signup (#4)
    * aah
    
    * sas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-  end
+    @user = User.find(params[:id])
+  end 
 
   # GET /users/new
   def new
@@ -22,15 +23,11 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+     else
+      render "new"
     end
   end
 
@@ -65,6 +62,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
