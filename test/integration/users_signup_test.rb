50dba787a4d2363097b60b42b22e4e0b63f2c446
ivanstiv <albertduf@mail.ru>
2024-03-16 08:29:21 +0300

    sas

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,14 +1,14 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-	test "invalid signup information" do
+	test "valid signup information" do
 	  get signup_path
-	  assert_no_difference "User.count" do
-	  	post users_path, user: { name: "",
-	  		email:"user@invalid",
+	  assert_no_difference "User.count", 1 do
+	  	post_via_redirect users_path, user: { name: "Example User",
+	  		email:"user@example.com",
 	  		password: "foo",
 	  		password_confirmation: "bar" }
 	  	end
-	  	assert_template 'users/new'
-	  end
+	  	assert_template 'users/show'
+	  end 
 end 
